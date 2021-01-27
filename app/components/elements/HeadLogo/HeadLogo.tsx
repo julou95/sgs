2a77097f8c25b3a2e6be1cc302104cfdc2d92674
julou95 <louis.jungo@microspot.ch>
2021-01-27 16:36:04 +0100

    mobile header fix

--- app/components/elements/HeadLogo/HeadLogo.tsx
+++ app/components/elements/HeadLogo/HeadLogo.tsx
@@ -1,9 +1,13 @@
+import cx from 'classnames';
 import styles from './HeadLogo.module.scss';
 
 const HeadLogo: React.FC = () => (
     <a href="#">
-        <div className={styles.text}>
+        <div className={cx(styles.text, styles.full)}>
             SNITCHES GET STITCHES
+        </div>
+        <div className={cx(styles.text, styles.small)}>
+            SGS
         </div>
     </a>
 );
